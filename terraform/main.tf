8451b9514b6ccf34a1f2db937a2ab6b92a16b29d
ccssh-eng <cirdec.labhub@gmail.com>
2026-02-15 09:16:16 +0100

    Mise à jour terraform k8s

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,30 @@
-provider "ovh" {
-  endpoint = "ovh-eu"
+terraform {
+  required_providers {
+    ovh = {
+      source  = "ovh/ovh"
+      version = ">= 0.35.0"
+    }
+  }
 }
 
-# Créer un cluster Kubernetes OVH
-resource "ovh_cloud_project_kube" "flask_cluster" {
-  name   = "flask-mongo-devsecops"
-  region = "GRA5" # exemple
-  node_count = 2
-  node_flavor = "b2-7"
-  kube_version = "1.27"
+provider "ovh" {
+  endpoint           = "ovh-eu"
+  application_key    = var.ovh_application_key
+  application_secret = var.ovh_application_secret
+  consumer_key       = var.ovh_consumer_key
+}
+
+resource "ovh_cloud_project_kube" "k8s" {
+  service_name = var.project_id
+  name         = "devsecops-k8s"
+  region       = var.region
+#  version      = "1.28"
+}
+
+resource "ovh_cloud_project_kube_nodepool" "nodepool" {
+  service_name  = var.project_id
+  kube_id       = ovh_cloud_project_kube.k8s.id
+  name          = "economy-pool"
+  flavor_name = "b2-7"
+  desired_nodes = 1
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "ovh_application_key" {}
+variable "ovh_application_secret" {}
+variable "ovh_consumer_key" {}
+variable "project_id" {}
+variable "region" {
+  default = "SBG5"
+}
