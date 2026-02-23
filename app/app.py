a55fdfce903f9203d4bbaab8bc63f4d1713e3a04
ccssh-eng <cirdec.labhub@gmail.com>
2026-02-23 16:09:04 +0100

    Mise à jour: app.py, flask-api.yaml et monitoring pour metrics

--- app/app.py
+++ app/app.py
@@ -1,35 +1,22 @@
 from flask import Flask, jsonify
 from app.db import get_collection
 from prometheus_client import Counter, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST
+from prometheus_client import start_http_server
 from flask import Response
 
 app = Flask(__name__)
 
-
 REQUEST_COUNT = Counter(
     "flask_requests_total",
-    "Total number of requests",
+    "Total number of HTTP requests",
     ["method", "endpoint"]
 )
 
-
-@app.route("/health", methods=["GET"])
-def health():
-    REQUEST_COUNT.labels(method="GET", endpoint="/health").inc()
-    return jsonify({"status": "UP"})
-
+@app.before_request
+def before_request():
+    REQUEST_COUNT.labels(method="GET", endpoint="/").inc()
 
 @app.route("/metrics")
 def metrics():
-    return Response(generate_latest(), mimetype="text/plain")
-
-
-@app.route("/items", methods=["GET"])
-def get_items():
-    col = get_collection()
-    items = list(col.find({}, {"_id": 0}))
-    return jsonify(items)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
