d862b7bc3e26332db4f2c9eb86a0fb98222fb40c
ccssh-eng <cirdec.labhub@gmail.com>
2026-02-23 17:07:05 +0100

    fix: pep8 + prometheus metrics

--- app/app.py
+++ app/app.py
@@ -2,7 +2,9 @@ from flask import Flask, jsonify, Response
 from app.db import get_collection
 from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
+
 app = Flask(__name__)
+
 
 REQUEST_COUNT = Counter(
     "flask_requests_total",
@@ -10,13 +12,16 @@ REQUEST_COUNT = Counter(
     ["method", "endpoint"]
 )
 
+
 @app.before_request
 def before_request():
     REQUEST_COUNT.labels(method="GET", endpoint="global").inc()
 
+
 @app.route("/health", methods=["GET"])
 def health():
     return jsonify({"status": "UP"})
+
 
 @app.route("/items", methods=["GET"])
 def get_items():
@@ -24,9 +29,11 @@ def get_items():
     items = list(col.find({}, {"_id": 0}))
     return jsonify(items)
 
+
 @app.route("/metrics")
 def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
